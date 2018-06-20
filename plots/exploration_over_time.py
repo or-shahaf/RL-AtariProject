e11450b700427e489f25ba1c3044c9b3e7f448ac
or-shahaf <or7shahaf+github@gmail.com>
2018-06-21 02:10:13 +0300

    rm exploration_over_time.py

--- plots/exploration_over_time.py
+++ plots/exploration_over_time.py
@@ -14,21 +14,36 @@ def get_explore_by_t(schedule):
 max_piece_timestamp = 1000002
 
 linear_explore_by_t = get_explore_by_t(utils.schedule.LinearSchedule(1000000, 0.1))
+linear05_explore_by_t = get_explore_by_t(utils.schedule.LinearSchedule(1055560, 0.05))
 greedy_linear_explore_by_t = get_explore_by_t(utils.schedule.LinearSchedule(511003, 0.1))
-inv_exp_explore_by_t = get_explore_by_t(utils.schedule.InverseExponentialSchedule(0.1, 1582985))
+nongreedy_linear_explore_by_t = get_explore_by_t(utils.schedule.LinearSchedule(1500000, 0.1))
+inv_exp1_explore_by_t = get_explore_by_t(utils.schedule.InverseExponentialSchedule(0.1, 1000000))
+#inv_exp2_explore_by_t = get_explore_by_t(utils.schedule.InverseExponentialSchedule(0.1, 1582985))
 const_explore_by_t = get_explore_by_t(utils.schedule.ConstantSchedule(0.05))
+const1_explore_by_t = get_explore_by_t(utils.schedule.ConstantSchedule(0.1))
 piece_explore_by_t = get_explore_by_t(utils.schedule.PiecewiseSchedule(
     [(0, 1), (max_piece_timestamp // 3, 0.95), (2 * max_piece_timestamp // 3, 0.15),
      (max_piece_timestamp, 0.1)], outside_value=0.1))
 
 
+line05, = plt.plot(t_array, linear05_explore_by_t, label="LinearSchedule(1055560, 0.1)")
 line, = plt.plot(t_array, linear_explore_by_t, label="LinearSchedule(1000000, 0.1)")
 greed_line, = plt.plot(t_array, greedy_linear_explore_by_t, label="LinearSchedule(511003, 0.1)")
-inv_exp, = plt.plot(t_array, inv_exp_explore_by_t, label="InverseExponentialSchedule(0.1, 1582985)")
+nongreed_line, = plt.plot(t_array, nongreedy_linear_explore_by_t, label="LinearSchedule(1500000, "
+                                                                        "0.1)")
+inv_exp1, = plt.plot(t_array, inv_exp1_explore_by_t, label="InverseExponentialSchedule(0.1, "
+                                                          "1000000)")
+#inv_exp2, = plt.plot(t_array, inv_exp2_explore_by_t, label="InverseExponentialSchedule(0.1, "
+#                                                          "1582985)")
+# not needed because already have greed_line and inv_exp1
 const, = plt.plot(t_array, const_explore_by_t, label="ConstantSchedule(0.05)")
+const1, = plt.plot(t_array, const1_explore_by_t, label="ConstantSchedule(0.1)")
 piece, = plt.plot(t_array, piece_explore_by_t, label="PiecewiseSchedule")
 plt.xlabel('t')
 plt.ylabel('exploration')
-plt.legend(handles=[line, greed_line, inv_exp, const, piece])
+plt.legend(handles=[line, line05, greed_line, nongreed_line, inv_exp1, const, const1,
+                    piece])
 plt.grid()
+plt.xlim(0, t_array.max())
+plt.ylim(0, 1.01)
 plt.savefig('./exploration.png')
